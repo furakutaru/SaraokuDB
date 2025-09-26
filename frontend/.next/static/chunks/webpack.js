a4224754e92f2d769aeed32ad2af9c8d71f24a0a
furakutaru <57737883+furakutaru@users.noreply.github.com>
2025-09-26 11:22:43 +0900

    詳細ページにURL、オークションの日付の追加

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "151ba9da39d02137"; }
+/******/ 		__webpack_require__.h = function() { return "73d82308a9d3e214"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
