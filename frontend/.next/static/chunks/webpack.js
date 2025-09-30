71430a49584261029fac8c6c55aaefb35784d5cf
furakutaru <57737883+furakutaru@users.noreply.github.com>
2025-09-30 13:58:54 +0900

    詳細ページのレイアウト調整

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bb9eebdf06606666"; }
+/******/ 		__webpack_require__.h = function() { return "dfe3b8e8fc92f6dd"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
