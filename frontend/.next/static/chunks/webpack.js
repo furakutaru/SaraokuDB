09ba2ccab80b90c4bc6519ef06e6ee99352f0d75
furakutaru <57737883+furakutaru@users.noreply.github.com>
2025-10-09 13:48:08 +0900

    詳細ページの落札価格のコンポーネント化

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ababc3bd21e08756"; }
+/******/ 		__webpack_require__.h = function() { return "e0279fb862136741"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
