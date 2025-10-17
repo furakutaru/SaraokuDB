3621be2a58addb9a673324183f1e0773579c1f9a
furakutaru <57737883+furakutaru@users.noreply.github.com>
2025-10-18 00:17:08 +0900

    エンドポイントの作成

--- api/api.py
+++ api/api.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from .health import router as health_router
 from .auth.login import router as login_router
+from .auth.token import router as token_router
 
 app = FastAPI()
 
 # ルーターを登録
 app.include_router(health_router, prefix="/api")
 app.include_router(login_router, prefix="/api")
+app.include_router(token_router, prefix="/api")
 
 # ルートエンドポイント
 @app.get("/")
