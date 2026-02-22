a76df307cf720db9e149457ab2e70eb20438bbc1
furakutaru <57737883+furakutaru@users.noreply.github.com>
2026-02-22 20:55:26 +0900

    相対参照を絶対参照に変更

--- backend/scrapers/__init__.py
+++ backend/scrapers/__init__.py
@@ -1,6 +1,6 @@
 # Scrapers package
 
-# Import from the local rakuten_scraper
-from scrapers.rakuten_scraper import RakutenAuctionScraper
+# Import with absolute package path to work under Docker/Render
+from backend.scrapers.rakuten_scraper import RakutenAuctionScraper
 
 __all__ = ['RakutenAuctionScraper']
